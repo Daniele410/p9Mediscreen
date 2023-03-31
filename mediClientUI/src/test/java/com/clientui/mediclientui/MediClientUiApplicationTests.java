77dc82992bd2ed06e76b6344fe91add8ab66f7a7
Daniele410 <miraglia.daniele410@gmail.com>
2023-03-31 15:41:00 +0200

    add mediClientUI module

---
+++ mediClientUI/src/test/java/com/clientui/mediclientui/MediClientUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientui.mediclientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediClientUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
