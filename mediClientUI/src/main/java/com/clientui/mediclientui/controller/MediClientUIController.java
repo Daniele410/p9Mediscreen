2b45b8cde698cbd49ac73066378f710b171a4502
Daniele410 <miraglia.daniele410@gmail.com>
2023-04-04 15:38:54 +0200

    fix projet

--- mediClientUI/src/main/java/com/clientui/mediclientui/controller/MediClientUIController.java
+++ mediClientUI/src/main/java/com/clientui/mediclientui/controller/MediClientUIController.java
@@ -2,9 +2,11 @@ package com.clientui.mediclientui.controller;
 
 import com.clientui.mediclientui.beans.PatientBean;
 import com.clientui.mediclientui.proxies.PatientProxy;
+import com.medipatient.medipatient.Exception.UserNotFoundException;
 import com.medipatient.medipatient.model.Patient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 public class MediClientUIController {
@@ -58,13 +62,25 @@ public class MediClientUIController {
 
     }
 
-    @GetMapping("/patientUpdateForm/{id}")
+    @GetMapping(value = "/patientUpdateForm/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id,Model model) {
         logger.debug("get request patient/update/{}", id);
         Patient patient = patientProxy.getPatient(id);
 
         model.addAttribute("patient", patient);
         return "patientUpdateForm";
+    }
+
+    @PostMapping(value = "/patientUpdateForm/{id}")
+    public String updatePatient(@PathVariable ("id") long id, @RequestBody @ModelAttribute("patient") PatientBean patientBean, BindingResult bindingResult,Model model) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/patientUpdate?error";
+        }
+
+        patientProxy.updatePatient(patientBean);
+        logger.info("update patient");
+        return "redirect:/patients?success";
+
     }
 
 
