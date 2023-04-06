e8e25b4bc89de299efbecd9affbd68db632e9181
Daniele410 <miraglia.daniele410@gmail.com>
2023-04-06 14:56:09 +0200

    add module mediNote

---
+++ mediNote/src/main/java/com/medinote/medinote/MediNoteApplication.java
@@ -0,0 +1,13 @@
+package com.medinote.medinote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediNoteApplication.class, args);
+	}
+
+}
---
+++ mediNote/src/test/java/com/medinote/medinote/MediNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinote.medinote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
