62e40e3e17df0b47fb8f95e68dab53f0093e36c1
Daniele410 <miraglia.daniele410@gmail.com>
2023-04-14 11:44:28 +0200

    add mediAssessement to project

---
+++ mediAssessment/src/main/java/com/mediassessment/mediassessment/MediAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.mediassessment.mediassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediAssessmentApplication.class, args);
+	}
+
+}
---
+++ mediAssessment/src/test/java/com/mediassessment/mediassessment/MediAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediassessment.mediassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
