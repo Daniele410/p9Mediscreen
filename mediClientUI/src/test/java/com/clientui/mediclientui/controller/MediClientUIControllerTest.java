a7d90a661240019b507dd73d1e05a27551de51f0
Daniele410 <miraglia.daniele410@gmail.com>
2023-04-14 11:27:02 +0200

    fix project

--- mediClientUI/src/test/java/com/clientui/mediclientui/controller/MediClientUIControllerTest.java
+++ mediClientUI/src/test/java/com/clientui/mediclientui/controller/MediClientUIControllerTest.java
@@ -5,12 +5,12 @@ import com.clientui.mediclientui.beans.PatientBean;
 import com.clientui.mediclientui.constant.Gender;
 import com.clientui.mediclientui.proxies.NoteProxy;
 import com.clientui.mediclientui.proxies.PatientProxy;
-import io.swagger.models.Model;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
 import java.time.LocalDate;
@@ -119,7 +119,7 @@ class MediClientUIControllerTest {
         String result = mediClientUIController.registerPatient(patient, bindingResult);
 
         //Then
-        assertEquals("redirect:/patientForm?error", result);
+        assertEquals("/patientForm", result);
         verify(bindingResult, times(1)).hasErrors();
     }
 
@@ -160,7 +160,14 @@ class MediClientUIControllerTest {
     @Test
     void updatePatientFormShouldReturnModifiedModelAndView_invalidData() {
         //Given
-        PatientBean patient = new PatientBean(1L, "Piero", "Brow", LocalDate.of(1084, 9, 10), Gender.MALE, "St Toto", "213213213213");
+        PatientBean patient = new PatientBean();
+        patient.setId(1L);
+        patient.setFirstName("Piero");
+        patient.setLastName("Brow");
+        patient.setBirthday(LocalDate.of(1084, 9, 10));
+        patient.setGender(Gender.MALE);
+        patient.setAddress("St Toto");
+        patient.setPhone("213213213213");
         when(bindingResult.hasErrors()).thenReturn(true);
 
         //When
@@ -168,7 +175,7 @@ class MediClientUIControllerTest {
         String result = mediClientUIController.updatePatient(1, patient, bindingResult, mock(org.springframework.ui.Model.class));
 
         //Then
-        assertEquals("redirect:/patientUpdate?error", result);
+        assertEquals("redirect:/patientUpdateForm?error", result);
         verify(bindingResult, times(1)).hasErrors();
     }
 
@@ -182,25 +189,169 @@ class MediClientUIControllerTest {
         verify(patientProxy, times(1)).deletePatient(1L);
     }
 
-//    @Test
-//    void showNotesShouldReturnModifiedModelAndView() {
-//        //Given
-//        List<NoteBean> allNotes = new ArrayList<>();
-//        NoteBean noteBean1 = new NoteBean("1234123", 1L, "Bros", LocalDate.of(1084, 9, 10));
-//        NoteBean noteBean2 = new NoteBean("0987654", 2L, "Bros", LocalDate.of(1986, 1, 18));
-//
-//        allNotes.add(noteBean1);
-//        allNotes.add(noteBean2);
-//
-//        when(noteProxy.getNoteByPatientId(1L)).thenReturn(allNotes);
-//
-//        //When
-//
-//        String result = mediClientUIController.patientNotes(mock(org.springframework.ui.Model.class));
-//
-//        //Then
-//        assertEquals("patientNotes", result);
-//
-//
-//    }
+    @Test
+    void patientNotes_ShouldReturnModifiedModelAndView() {
+        //Given
+        PatientBean patient = new PatientBean(1L, "Piero", "Brow", LocalDate.of(1084, 9, 10), Gender.MALE, "St Toto", "213213213213");
+
+        List<NoteBean> allNotes = new ArrayList<>();
+        NoteBean noteBean1 = new NoteBean("1234123", 1L, "Bros", LocalDate.of(1084, 9, 10));
+        NoteBean noteBean2 = new NoteBean("0987654", 2L, "Bros", LocalDate.of(1986, 1, 18));
+
+        allNotes.add(noteBean1);
+        allNotes.add(noteBean2);
+        when(patientProxy.getPatient(1L)).thenReturn(patient);
+        when(noteProxy.getNoteByPatientId(1L)).thenReturn(allNotes);
+
+        //When
+
+        String result = mediClientUIController.patientNotes(1L,mock(org.springframework.ui.Model.class),patient);
+
+        //Then
+        assertEquals("patientNotes", result);
+
+
+    }
+
+    @Test
+    void showAddNoteForm_ShouldReturnModifiedModelAndView() {
+        //Given
+        PatientBean patient = new PatientBean(1L, "Piero", "Brow", LocalDate.of(1084, 9, 10), Gender.MALE, "St Toto", "213213213213");
+
+        List<NoteBean> allNotes = new ArrayList<>();
+        NoteBean noteBean1 = new NoteBean("1234123", 1L, "Bros", LocalDate.of(1084, 9, 10));
+        NoteBean noteBean2 = new NoteBean("0987654", 2L, "Bros", LocalDate.of(1986, 1, 18));
+
+        allNotes.add(noteBean1);
+        allNotes.add(noteBean2);
+        when(patientProxy.getPatient(1L)).thenReturn(patient);
+
+
+        //When
+        String result = mediClientUIController.showAddNoteForm(noteBean1,mock(org.springframework.ui.Model.class),1L);
+
+        //Then
+        assertEquals("noteForm", result);
+
+
+    }
+
+    @Test
+    void registerNotePatient_ShouldReturnModifiedModelAndViewBindingResultErrorFalse() {
+        //Given
+        PatientBean patient = new PatientBean(1L, "Piero", "Brow", LocalDate.of(1084, 9, 10), Gender.MALE, "St Toto", "213213213213");
+
+        List<NoteBean> allNotes = new ArrayList<>();
+        NoteBean noteBean1 = new NoteBean("1234123", 1L, "Bros", LocalDate.of(1084, 9, 10));
+        NoteBean noteBean2 = new NoteBean("0987654", 2L, "Bros", LocalDate.of(1986, 1, 18));
+
+        allNotes.add(noteBean1);
+        allNotes.add(noteBean2);
+        when(bindingResult.hasErrors()).thenReturn(false);
+
+        //When
+        String result = mediClientUIController.registerNotePatient(noteBean1,bindingResult);
+
+        //Then
+        assertEquals("redirect:/patient/note/{patientId}", result);
+
+
+    }
+
+    @Test
+    void registerNotePatient_ShouldReturnModifiedModelAndViewBindingResultErrorTrue() {
+        //Given
+        PatientBean patient = new PatientBean(1L, "Piero", "Brow", LocalDate.of(1084, 9, 10), Gender.MALE, "St Toto", "213213213213");
+
+        List<NoteBean> allNotes = new ArrayList<>();
+        NoteBean noteBean1 = new NoteBean("1234123", 1L, "Bros", LocalDate.of(1084, 9, 10));
+        NoteBean noteBean2 = new NoteBean("0987654", 2L, "Bros", LocalDate.of(1986, 1, 18));
+
+        allNotes.add(noteBean1);
+        allNotes.add(noteBean2);
+        when(bindingResult.hasErrors()).thenReturn(true);
+
+        //When
+        String result = mediClientUIController.registerNotePatient(noteBean1,bindingResult);
+
+        //Then
+        assertEquals("redirect:/noteForm/{patientId}?error", result);
+
+    }
+
+    @Test
+    void showUpdateNoteForm_ShouldReturnModifiedModelAndView() {
+        //Given
+        NoteBean noteBean1 = new NoteBean("1234123", 1L, "Bros", LocalDate.of(1084, 9, 10));
+        NoteBean noteBean2 = new NoteBean("0987654", 2L, "Bros", LocalDate.of(1986, 1, 18));
+
+        when(noteProxy.getNoteById(noteBean1.getId())).thenReturn(noteBean1);
+
+
+        //When
+        String result = mediClientUIController.showUpdateNoteForm(noteBean1.getId(),mock(org.springframework.ui.Model.class),noteBean1);
+
+        //Then
+        assertEquals("noteUpdateForm", result);
+
+    }
+
+    @Test
+    void updateNote_ShouldReturnModifiedModelAndViewBindingResultErrorFalse() {
+        //Given
+
+        List<NoteBean> allNotes = new ArrayList<>();
+        NoteBean noteBean1 = new NoteBean("1234123", 1L, "Bros", LocalDate.of(1084, 9, 10));
+        NoteBean noteBean2 = new NoteBean("0987654", 2L, "Bros", LocalDate.of(1986, 1, 18));
+
+        allNotes.add(noteBean1);
+        allNotes.add(noteBean2);
+        when(bindingResult.hasErrors()).thenReturn(false);
+        when(noteProxy.updateNote(noteBean1)).thenReturn(noteBean1);
+
+        //When
+        String result = mediClientUIController.updateNote(noteBean1.getId(),noteBean1,bindingResult,model);
+
+        //Then
+        assertEquals("redirect:/noteUpdateForm/{id}?success", result);
+
+
+    }
+    @Test
+    void updateNote_ShouldReturnModifiedModelAndViewBindingResultErrorTrue() {
+        //Given
+        NoteBean noteBean1 = new NoteBean();
+        noteBean1.setId("1234123");
+        noteBean1.setPatientId(1L);
+        noteBean1.setMessage("My note");
+        noteBean1.setDate(LocalDate.of(1084, 9, 10));
+
+        when(bindingResult.hasErrors()).thenReturn(true);
+
+        //When
+        String result = mediClientUIController.updateNote(noteBean1.getId(),noteBean1,bindingResult,model);
+
+        //Then
+        assertEquals("redirect:/noteUpdateForm/{id}?error", result);
+
+    }
+
+    @Test
+    void deleteNote_ShouldReturnModifiedModelAndViewBindingResultError() {
+        //Given
+        NoteBean noteBean1 = new NoteBean("1234123", 1L, "Bros", LocalDate.of(1084, 9, 10));
+        when(noteProxy.getNoteById(noteBean1.getId())).thenReturn(noteBean1);
+        when(noteProxy.deleteNote(noteBean1.getId())).thenReturn(noteBean1);
+
+        //When
+        String result = mediClientUIController.deleteNote(noteBean1.getId());
+
+        //Then
+        assertEquals("redirect:/patient/note/"+noteBean1.getPatientId()+"?successDelete", result);
+
+
+    }
+
+
+
 }
