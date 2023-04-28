8ff2a9713cabf827f815da62ea64a13d3b92b6e5
Daniele410 <miraglia.daniele410@gmail.com>
2023-04-28 11:34:35 +0200

    merge

--- mediAssessment/src/main/java/com/mediassessment/mediassessment/controller/AssessmentController.java
+++ mediAssessment/src/main/java/com/mediassessment/mediassessment/controller/AssessmentController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.springframework.http.HttpStatus.OK;
@@ -39,9 +41,19 @@ public class AssessmentController {
      */
     @GetMapping("/assess")
     public ResponseEntity<PatientBeanDto> getRapportAssessmentById (@RequestParam Long id){
-        log.info("get rapport by patientId :{} ", id);
+        log.info("get report by patientId :{} ", id);
         return new ResponseEntity<>(assessmentService.getRapportById((id)), OK);
 
+    }
+
+    @PostMapping(value = "/assess/id")
+    public ResponseEntity<String> getCurlReportById(Long patId) {
+        log.info(" get report request for patient id {} ",patId);
+        PatientBeanDto patientBeanDto = assessmentService.getRapportById(patId);
+        String report = "Patient : " + patientBeanDto.getFirstName() +
+                " (age :" + patientBeanDto.getAge() + ") diabetes assessment is: "
+                + patientBeanDto.getRiskLevel().toString();
+        return new ResponseEntity<>(report, OK);
     }
 
 }
---
+++ mediAssessment/src/main/java/com/mediassessment/mediassessment/curl/AssessmentCurlController.java
@@ -0,0 +1,20 @@
+package com.mediassessment.mediassessment.curl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AssessmentCurlController {
+
+    /**
+     * SLF4J Logger instance.
+     */
+    private final Logger log = LoggerFactory.getLogger(AssessmentCurlController.class);
+
+    /**
+     * Instance of IPatientService
+     */
+}
