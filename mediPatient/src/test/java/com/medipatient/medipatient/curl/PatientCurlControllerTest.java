db6b04e93292dbb5a925e05a001d1a8f787f9ab1
Daniele410 <miraglia.daniele410@gmail.com>
2023-05-03 16:28:04 +0200

    fix tests

--- mediPatient/src/test/java/com/medipatient/medipatient/curl/PatientCurlControllerTest.java
+++ mediPatient/src/test/java/com/medipatient/medipatient/curl/PatientCurlControllerTest.java
@@ -1,13 +1,24 @@
 package com.medipatient.medipatient.curl;
 
+import com.medipatient.medipatient.constant.Gender;
+import com.medipatient.medipatient.model.Patient;
+import com.medipatient.medipatient.model.dto.PatientDto;
 import com.medipatient.medipatient.service.PatientServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class PatientCurlControllerTest {
     @InjectMocks
@@ -16,8 +27,47 @@ class PatientCurlControllerTest {
     PatientServiceImpl patientService;
 
     @Test
-    void addPatientCurlShouldReturnModifiedModelAndView() {
+    void addPatientCurlShouldReturnModifiedModelAndViewCaseMale() {
+        //Given
+        PatientDto patientDto=new PatientDto();
+        patientDto.setId(1L);
+        patientDto.setFamily("Mariottide");
+        patientDto.setGiven("Nando");
+        patientDto.setDob("1954-10-23");
+        patientDto.setAddress("my address");
+        patientDto.setPhone("000111333222");
+        patientDto.setSex(String.valueOf(Gender.MALE));
+        Patient patient = new Patient(patientDto.getId(), patientDto.getFamily(), patientDto.getGiven(),  LocalDate.parse(patientDto.getDob()), patientDto.getSex()
+                .equalsIgnoreCase("F") ? Gender.FEMALE : Gender.MALE, patientDto.getAddress(), patientDto.getPhone());
+       when(patientService.savePatient(any(Patient.class))).thenReturn(patient);
 
+       //When
+        ResponseEntity<Patient> result = patientCurlController.addPatientCurl(patientDto);
+
+        //Then
+        assertEquals(result.getStatusCode(), HttpStatus.CREATED);
+
+    }
+    @Test
+    void addPatientCurlShouldReturnModifiedModelAndViewCaseFemale() {
+        //Given
+        PatientDto patientDto=new PatientDto();
+        patientDto.setId(1L);
+        patientDto.setFamily("Mariottide");
+        patientDto.setGiven("Nando");
+        patientDto.setDob("1954-10-23");
+        patientDto.setAddress("my address");
+        patientDto.setPhone("000111333222");
+        patientDto.setSex(String.valueOf(Gender.FEMALE));
+        Patient patient = new Patient(patientDto.getId(), patientDto.getFamily(), patientDto.getGiven(),  LocalDate.parse(patientDto.getDob()), patientDto.getSex()
+                .equalsIgnoreCase("F") ? Gender.FEMALE : Gender.MALE, patientDto.getAddress(), patientDto.getPhone());
+        when(patientService.savePatient(any(Patient.class))).thenReturn(patient);
+
+        //When
+        ResponseEntity<Patient> result = patientCurlController.addPatientCurl(patientDto);
+
+        //Then
+        assertEquals(result.getStatusCode(), HttpStatus.CREATED);
 
     }
 }
