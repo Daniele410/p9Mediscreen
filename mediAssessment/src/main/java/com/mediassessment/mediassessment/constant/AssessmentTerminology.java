f2a3bc88e95a73b981ff2bb7248787a92c3b5c6f
Daniele410 <miraglia.daniele410@gmail.com>
2023-05-08 19:19:02 +0200

    fix

--- mediAssessment/src/main/java/com/mediassessment/mediassessment/constant/AssessmentTerminology.java
+++ mediAssessment/src/main/java/com/mediassessment/mediassessment/constant/AssessmentTerminology.java
@@ -15,7 +15,7 @@ public class AssessmentTerminology {
      * english terminology to diagnostic
      */
 
-    public static List<String> TERMINOLOGY = List.of("Hemoglobin"," A1C","Microalbumin","Height","Weight",
+    public static List<String> TERMINOLOGY = List.of("Hemoglobin","A1C","Microalbumin","Height","Weight",
             "Smoker","Abnormal","Cholesterol","Dizziness","Relapse","Reaction","Antibody");
 
 }
